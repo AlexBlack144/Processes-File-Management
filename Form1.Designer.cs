801f4ea0046d7c018b8d9b0f771a36a812b952c7
AlexBlack144 <aboneofficial@gmail.com>
2023-05-07 22:57:22 +0300

    Creat Project
    File Management (C#, WinForm, Processes)

---
+++ Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace ExamTask
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBoxPath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBoxFilter = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonScan = new System.Windows.Forms.Button();
+            this.buttonPaid = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeaderID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderDateTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderFileName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderPath = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxPath
+            // 
+            this.textBoxPath.Location = new System.Drawing.Point(12, 32);
+            this.textBoxPath.Name = "textBoxPath";
+            this.textBoxPath.Size = new System.Drawing.Size(161, 20);
+            this.textBoxPath.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Location = new System.Drawing.Point(179, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(51, 52);
+            this.button1.TabIndex = 1;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button_Click_Folder);
+            // 
+            // textBoxFilter
+            // 
+            this.textBoxFilter.Location = new System.Drawing.Point(236, 32);
+            this.textBoxFilter.Name = "textBoxFilter";
+            this.textBoxFilter.Size = new System.Drawing.Size(161, 20);
+            this.textBoxFilter.TabIndex = 2;
+            this.textBoxFilter.TextChanged += new System.EventHandler(this.textBoxFilter_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Folders:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(661, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Actions";
+            // 
+            // buttonScan
+            // 
+            this.buttonScan.Location = new System.Drawing.Point(664, 65);
+            this.buttonScan.Name = "buttonScan";
+            this.buttonScan.Size = new System.Drawing.Size(124, 40);
+            this.buttonScan.TabIndex = 6;
+            this.buttonScan.Text = "Scan";
+            this.buttonScan.UseVisualStyleBackColor = true;
+            this.buttonScan.Click += new System.EventHandler(this.button_Click_Scan);
+            // 
+            // buttonPaid
+            // 
+            this.buttonPaid.Location = new System.Drawing.Point(664, 140);
+            this.buttonPaid.Name = "buttonPaid";
+            this.buttonPaid.Size = new System.Drawing.Size(124, 40);
+            this.buttonPaid.TabIndex = 8;
+            this.buttonPaid.Text = "Paid version";
+            this.buttonPaid.UseVisualStyleBackColor = true;
+            this.buttonPaid.Click += new System.EventHandler(this.button_Click_Paid);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(664, 186);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(124, 40);
+            this.buttonDelete.TabIndex = 9;
+            this.buttonDelete.Text = "Delete (paid versione)";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.button_Click_Delete);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderID,
+            this.columnHeaderDateTime,
+            this.columnHeaderSize,
+            this.columnHeaderFileName,
+            this.columnHeaderPath});
+            this.listView1.FullRowSelect = true;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 65);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(646, 382);
+            this.listView1.TabIndex = 11;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeaderID
+            // 
+            this.columnHeaderID.Text = "№";
+            this.columnHeaderID.Width = 25;
+            // 
+            // columnHeaderDateTime
+            // 
+            this.columnHeaderDateTime.Text = "Date and Time";
+            this.columnHeaderDateTime.Width = 100;
+            // 
+            // columnHeaderSize
+            // 
+            this.columnHeaderSize.Text = "Size";
+            // 
+            // columnHeaderFileName
+            // 
+            this.columnHeaderFileName.Text = "File Name";
+            this.columnHeaderFileName.Width = 180;
+            // 
+            // columnHeaderPath
+            // 
+            this.columnHeaderPath.Text = "Path";
+            this.columnHeaderPath.Width = 238;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(233, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(137, 13);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Filter of files (paid versione):";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonPaid);
+            this.Controls.Add(this.buttonScan);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxFilter);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBoxPath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxPath;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBoxFilter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonScan;
+        private System.Windows.Forms.Button buttonPaid;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeaderID;
+        private System.Windows.Forms.ColumnHeader columnHeaderDateTime;
+        private System.Windows.Forms.ColumnHeader columnHeaderSize;
+        private System.Windows.Forms.ColumnHeader columnHeaderFileName;
+        public System.Windows.Forms.ColumnHeader columnHeaderPath;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using Microsoft.VisualBasic;
+using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Windows.Forms;
+
+
+namespace ExamTask
+{
+    public partial class Form1 : Form
+    {
+        FolderBrowserDialog folderBrowserDialog;
+        RegistryKey key;
+        RegistryKey myProgKey;
+
+        public Form1()
+        {
+            InitializeComponent();
+            folderBrowserDialog = new FolderBrowserDialog();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            buttonDelete.Enabled = false;
+            textBoxFilter.Enabled = false;
+
+            key = Registry.CurrentUser;
+            myProgKey = key.CreateSubKey("MyProg");
+
+            //add key
+            myProgKey.SetValue("license", "admin");
+        }
+
+        private void textBoxFilter_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string filter = textBoxFilter.Text;
+                ScanList(filter);
+            }
+            catch (Exception) { }
+
+        }
+
+
+        #region Buttons
+        private void button_Click_Folder(object sender, EventArgs e)
+        {
+            OpenFolder();
+        }
+
+        private void button_Click_Scan(object sender, EventArgs e)
+        {
+
+            try
+            {
+                ScanList();
+            }
+            catch (Exception) { }
+        }
+
+        private void button_Click_Delete(object sender, EventArgs e)
+        {
+            try
+            {
+                if (listView1.Items.Count>0)
+                {
+                    string message = "\tAre you sure, you want to delete \n\t   this file from your PC?";
+                    string title = "Delete file";
+                    MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+                    DialogResult result = MessageBox.Show(message, title, buttons);
+                    if (result == DialogResult.Yes)
+                    {
+                        string fileToKill = listView1.SelectedItems[0].SubItems[4].Text;
+                        DeleteFile(fileToKill.ToString());
+                        ScanList();
+                    }
+                }
+
+            }
+            catch (Exception) { }           
+        }
+
+        private void button_Click_Paid(object sender, EventArgs e)
+        {
+            string key = Interaction.InputBox("Key: admin", "Enter the license key!");
+            try
+            {
+                //get key
+                var data = myProgKey.GetValue("license");
+
+                if (key == data.ToString())
+                {
+                    buttonDelete.Enabled = true;
+                    textBoxFilter.Enabled = true;
+                }
+            }
+            catch (Exception) { }
+        }
+        #endregion
+
+
+        #region Methods
+        static long GetFileSize(string FilePath)
+        {
+            if (File.Exists(FilePath))
+            {
+                return new FileInfo(FilePath).Length;
+            }
+            return 0;
+        }
+        private void ScanList()
+        {
+
+            listView1.Items.Clear();
+
+            int id_index = 0;
+            string path = textBoxPath.Text;
+
+            DirectoryInfo di = new DirectoryInfo(path);
+            FileInfo[] files = di.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                id_index++;
+                long fileSizeibBytes = GetFileSize(file.FullName);
+                long fileSizeibMbs = fileSizeibBytes / (1024 * 1024);
+
+                string[] row = new string[] { id_index.ToString(), file.LastWriteTime.ToString(), fileSizeibMbs.ToString() + " Mb", file.Name, file.FullName };
+
+                listView1.Items.Add(new ListViewItem(row));
+            }
+        }
+        private void ScanList(string filter)
+        {
+            try
+            {
+                listView1.Items.Clear();
+
+                int id_index = 0;
+                string path = textBoxPath.Text;
+
+                DirectoryInfo di = new DirectoryInfo(path);
+                FileInfo[] files = di.GetFiles("*." + filter);
+                foreach (FileInfo file in files)
+                {
+
+                    id_index++;
+                    long fileSizeibBytes = GetFileSize(file.FullName);
+                    long fileSizeibMbs = fileSizeibBytes / (1024 * 1024);
+
+                    string[] row = new string[] { id_index.ToString(), file.LastWriteTime.ToString(), fileSizeibMbs.ToString() + " Mb", file.Name, file.FullName };
+
+                    listView1.Items.Add(new ListViewItem(row));
+                }
+            }
+            catch (Exception) { }
+        }
+
+        private void OpenFolder()
+        {
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filetext = folderBrowserDialog.SelectedPath;
+                textBoxPath.Text = filetext;
+            }
+        }
+        private void DeleteFile(string path)
+        {
+            File.Delete(path);
+        }
+
+        #endregion
+    }
+}
